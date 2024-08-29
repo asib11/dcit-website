30fcca51b6a558053da43a43ef968dd1b2592c86
asib ahmed <ahmedasib1@gmail.com>
2024-08-29 17:24:23 +0600

    feat: make navbar and set router

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+
+  RouterProvider,
+} from "react-router-dom";
+import router from './Routes/Routes.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <div className='max-w-7xl mx-auto'>
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>,
+  </div>
 )
