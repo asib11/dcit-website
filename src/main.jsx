d851a26171fc20f4109dc04c12f7bef37270c759
asib ahmed <ahmedasib1@gmail.com>
2024-08-31 17:08:08 +0600

    feat: navbar complete

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import {
 import router from './Routes/Routes.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <div className='max-w-7xl mx-auto'>
+  <div className=''>
     <StrictMode>
       <RouterProvider router={router} />
     </StrictMode>,
--- src/pages/shared/Navbar/Navbar.jsx
+++ src/pages/shared/Navbar/Navbar.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../../../assets/logo.png'
+import basisLogo from '../../../assets/basis-logo.svg'
+import { DevicePhoneMobileIcon, EnvelopeIcon } from '@heroicons/react/24/solid'
 
 const Navbar = () => {
     const navbarItem = <>
@@ -40,22 +43,46 @@ const Navbar = () => {
     </>
     return (
 
-        <div className="navbar bg-base-100">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+        <div className='max-w-7xl mx-auto relative'>
+            <div className=' text-[18px] flex justify-between items-center'>
+                <div><img src={logo} alt="" /></div>
+                <div className="p-4 flex  font-semibold" >
+                    <DevicePhoneMobileIcon className='size-12 rounded-full border-4 p-1 text-[#399918] border-[#399918] mr-6' />
+                    <div >
+                        <p className='hover:text-[#399918]'>017xxxxxxxxxx</p>
+                        <p className='hover:text-[#399918]'>017xxxxxxxxxx</p>
                     </div>
-                    <ul
-                        tabIndex={0}
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                        {navbarItem}
-                    </ul>
+                </div>
+                <div className="p-4 flex  font-semibold" >
+                    <DevicePhoneMobileIcon className='size-12 rounded-full border-4 p-1 text-[#399918] border-[#399918] mr-6' />
+                    <div >
+                        <p className='hover:text-[#399918]'>asib.bubt@gmail.com</p>
+                        <p className='hover:text-[#399918]'>asib.bubt@gmail.com</p>
+                    </div>
+                </div>
+                <div>
+                    <img src={basisLogo} alt="" />
+                    <p className='text-[#399918] mt-2 font-bold text-xl'>sitemap</p>
                 </div>
             </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1 ">
-                    {/* <li><a>Item 1</a></li>
+            <div className='shadow-lg p-4 fixed '>
+                <div className="navbar bg-base-100">
+                    <div className="navbar-start">
+                        <div className="dropdown">
+                            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                            </div>
+                            <ul
+                                tabIndex={0}
+                                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                                {navbarItem}
+                            </ul>
+                        </div>
+
+                    </div>
+                    <div className="navbar-center hidden lg:flex ">
+                        <ul className="menu menu-horizontal px-1 text-lg font-bold">
+                            {/* <li><a>Item 1</a></li>
                     <li>
                         <details>
                             <summary>Parent</summary>
@@ -66,11 +93,13 @@ const Navbar = () => {
                         </details>
                     </li>
                     <li><a>Item 3</a></li> */}
-                    {navbarItem}
-                </ul>
-            </div>
-            <div className="navbar-end">
-                <a className="btn bg-sky-400 text-white">Admisssion Now </a>
+                            {navbarItem}
+                        </ul>
+                    </div>
+                    <div className="navbar-end">
+                        <a className="btn bg-[#399918] text-white">Admisssion Now </a>
+                    </div>
+                </div>
             </div>
         </div>
     );
