bf96f2be1f7e857b05ca606c0b3d549c60fa489c
asib ahmed <ahmedasib1@gmail.com>
2024-09-02 16:57:08 +0600

    feat: recommeded courses

--- src/pages/Home/Recommend/Recommed.jsx
+++ src/pages/Home/Recommend/Recommed.jsx
@@ -2,20 +2,155 @@ import React from 'react';
 
 const Recommed = () => {
     return (
-        <div className='bg-'>
-            <div className="card bg-base-100 w-96 shadow-xl">
-                <figure>
-                    <img
-                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                        alt="Shoes" />
-                </figure>
-                <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
+        <div className='bg-[#e9ffec]'>
+            <h3 className='text-3xl text-center font-bold pt-16 '>RECOMMENDED</h3>
+            <h2 className=' text-5xl text-center font-extrabold my-6'>Course Programs</h2>
+            <div className='grid grid-cols-2 gap-6 md:grid-cols-3 max-w-7xl mx-auto py-20'>
+                <div className="card bg-base-100 w-96 shadow-xl">
+                    <figure>
+                        <img
+                            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                            alt="Shoes" />
+                    </figure>
+                    <div className="card-body">
+                        <p className='text-[#f58d5d] font-bold text-lg'>৳ 10,000.00 – ৳ 12,500.00</p>
+                        <p className='font-bold text-xl'>Graphics Design</p>
+                        
                     </div>
                 </div>
+                <div className="card bg-base-100 w-96 shadow-xl">
+                    <figure>
+                        <img
+                            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                            alt="Shoes" />
+                    </figure>
+                    <div className="card-body">
+                        <p className='text-[#f58d5d] font-bold text-lg'>৳ 10,000.00 – ৳ 12,500.00</p>
+                        <p className='font-bold text-xl'>Graphics Design</p>
+                        
+                    </div>
+                </div>
+                <div className="card bg-base-100 w-96 shadow-xl">
+                    <figure>
+                        <img
+                            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                            alt="Shoes" />
+                    </figure>
+                    <div className="card-body">
+                        <p className='text-[#f58d5d] font-bold text-lg'>৳ 10,000.00 – ৳ 12,500.00</p>
+                        <p className='font-bold text-xl'>Graphics Design</p>
+                        
+                    </div>
+                </div>
+                <div className="card bg-base-100 w-96 shadow-xl">
+                    <figure>
+                        <img
+                            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                            alt="Shoes" />
+                    </figure>
+                    <div className="card-body">
+                        <p className='text-[#f58d5d] font-bold text-lg'>৳ 10,000.00 – ৳ 12,500.00</p>
+                        <p className='font-bold text-xl'>Graphics Design</p>
+                        
+                    </div>
+                </div>
+                <div className="card bg-base-100 w-96 shadow-xl">
+                    <figure>
+                        <img
+                            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                            alt="Shoes" />
+                    </figure>
+                    <div className="card-body">
+                        <p className='text-[#f58d5d] font-bold text-lg'>৳ 10,000.00 – ৳ 12,500.00</p>
+                        <p className='font-bold text-xl'>Graphics Design</p>
+                        
+                    </div>
+                </div>
+                <div className="card bg-base-100 w-96 shadow-xl">
+                    <figure>
+                        <img
+                            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                            alt="Shoes" />
+                    </figure>
+                    <div className="card-body">
+                        <p className='text-[#f58d5d] font-bold text-lg'>৳ 10,000.00 – ৳ 12,500.00</p>
+                        <p className='font-bold text-xl'>Graphics Design</p>
+                        
+                    </div>
+                </div>
+                <div className="card bg-base-100 w-96 shadow-xl">
+                    <figure>
+                        <img
+                            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                            alt="Shoes" />
+                    </figure>
+                    <div className="card-body">
+                        <p className='text-[#f58d5d] font-bold text-lg'>৳ 10,000.00 – ৳ 12,500.00</p>
+                        <p className='font-bold text-xl'>Graphics Design</p>
+                        
+                    </div>
+                </div>
+                <div className="card bg-base-100 w-96 shadow-xl">
+                    <figure>
+                        <img
+                            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                            alt="Shoes" />
+                    </figure>
+                    <div className="card-body">
+                        <p className='text-[#f58d5d] font-bold text-lg'>৳ 10,000.00 – ৳ 12,500.00</p>
+                        <p className='font-bold text-xl'>Graphics Design</p>
+                        
+                    </div>
+                </div>
+                <div className="card bg-base-100 w-96 shadow-xl">
+                    <figure>
+                        <img
+                            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                            alt="Shoes" />
+                    </figure>
+                    <div className="card-body">
+                        <p className='text-[#f58d5d] font-bold text-lg'>৳ 10,000.00 – ৳ 12,500.00</p>
+                        <p className='font-bold text-xl'>Graphics Design</p>
+                        
+                    </div>
+                </div>
+                <div className="card bg-base-100 w-96 shadow-xl">
+                    <figure>
+                        <img
+                            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                            alt="Shoes" />
+                    </figure>
+                    <div className="card-body">
+                        <p className='text-[#f58d5d] font-bold text-lg'>৳ 10,000.00 – ৳ 12,500.00</p>
+                        <p className='font-bold text-xl'>Graphics Design</p>
+                        
+                    </div>
+                </div>
+                <div className="card bg-base-100 w-96 shadow-xl">
+                    <figure>
+                        <img
+                            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                            alt="Shoes" />
+                    </figure>
+                    <div className="card-body">
+                        <p className='text-[#f58d5d] font-bold text-lg'>৳ 10,000.00 – ৳ 12,500.00</p>
+                        <p className='font-bold text-xl'>Graphics Design</p>
+                        
+                    </div>
+                </div>
+                <div className="card bg-base-100 w-96 shadow-xl">
+                    <figure>
+                        <img
+                            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                            alt="Shoes" />
+                    </figure>
+                    <div className="card-body">
+                        <p className='text-[#f58d5d] font-bold text-lg'>৳ 10,000.00 – ৳ 12,500.00</p>
+                        <p className='font-bold text-xl'>Graphics Design</p>
+                        
+                    </div>
+                </div>
+                
             </div>
         </div>
     );
