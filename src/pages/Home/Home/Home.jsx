5b33246dc8d405bc63075cc9641abeb7ad82c61e
asib ahmed <ahmedasib1@gmail.com>
2024-09-03 13:07:33 +0600

    feat: facilities section added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -5,6 +5,7 @@ import Approve from '../Approved/Approve';
 import Recommed from '../Recommend/Recommed';
 import Forth from '../Forth/Forth';
 import Regular from '../Regular/Regular';
+import Facilities from '../Facilities/Facilities';
 
 const Home = () => {
     return (
@@ -15,6 +16,7 @@ const Home = () => {
             <Recommed></Recommed>
             <Forth></Forth>
             <Regular></Regular>
+            <Facilities></Facilities>
         </div>
     );
 };
---
+++ src/pages/Home/Team/Team.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Team = () => {
+    return (
+        <div>
+            <h2 className=' text-5xl text-center font-extrabold  pt-6 pb-20'>Our Facilities</h2>
+        </div>
+    );
+};
+
+export default Team;
