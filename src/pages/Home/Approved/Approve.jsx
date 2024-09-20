e996a8efc06a4ee8d7ba4e4d3aa7ab3d94210c63
asib ahmed <ahmedasib1@gmail.com>
2024-09-20 23:37:52 +0600

    fix: approve design

--- src/pages/Home/Approved/Approve.jsx
+++ src/pages/Home/Approved/Approve.jsx
@@ -17,24 +17,46 @@ const Approve = () => {
     return (
         <div className='bg-[#e6e7e7] p-16'>
             <div className='flex justify-between max-w-7xl mx-auto flex-wrap max-[992px]:gap-6'>
-                <div className='shadow-md border-4 border-[#399918] shadow-[#399918] p-10 rounded-lg w-96 bg-white' data-aos="flip-left">
-                    <h3 className='text-3xl font-bold mb-6 text-center'>Collaboration with</h3>
-                    <div className='flex flex-col justify-center items-center'><img src={collaboration2} alt="" />
-                        <img src={collaboration1} alt="" />
+                <div className='w-96 animate-border inline-block rounded-md bg-white bg-gradient-to-r from-red-500 via-green-500 to-yellow-500 bg-[length:400%_400%] p-1' data-aos="flip-left">
+                    <div className=' rounded-md bg-white px-5 py-3 font-bold h-full'>
+                        <h3 className='text-3xl font-bold mb-6 text-center'>Collaboration with</h3>
+                        <div className='flex flex-col justify-center items-center'><img src={collaboration2} alt="" />
+                            <img src={collaboration1} alt="" />
+                        </div>
                     </div>
                 </div>
-                <div className='shadow-md border-4 border-[#399918] shadow-[#399918] p-10 rounded-lg w-96 h-[436px] bg-white' data-aos="flip-up">
+                {/* <div className='shadow-md border-4 border-[#399918] shadow-[#399918] p-10 rounded-lg w-96 h-[436px] bg-white'       data-aos="flip-up">
+                    
                     <h3 className='text-3xl font-bold mb-6 text-center'>Approved by</h3>
                     <div className='flex justify-center items-center'>
                         <img src={approve1} alt="" />
                     </div>
+                </div> */}
+                <div className='w-96 animate-border inline-block rounded-md bg-white bg-gradient-to-r from-red-500 via-green-500 to-yellow-500 bg-[length:400%_400%] p-1' data-aos="flip-left">
+                    <div className=' rounded-md bg-white px-5 py-3 font-bold h-full '>
+                        <h3 className='text-3xl font-bold mb-6 text-center'>Approved by</h3>
+                        <div className='flex justify-center items-center'>
+                            <img  src={approve1} alt="" />
+                            {/* <img src={collaboration1} alt="" /> */}
+                        </div>
+                    </div>
                 </div>
-                <div className='shadow-md border-4 border-[#399918] shadow-[#399918]  p-10 rounded-lg w-96 bg-white' data-aos="flip-right">
+                {/* <div className='shadow-md border-4 border-[#399918] shadow-[#399918]  p-10 rounded-lg w-96 bg-white' data-aos="flip-right">
                     <h3 className='text-3xl font-bold mb-6 text-center'>Member of</h3>
                     <div className='flex flex-col justify-center items-center'>
                         <img src={member1} alt="" />
                         <div className='w-80'><img src={member2} alt="" /></div>
                         <img src={member3} alt="" />
+                    </div>
+                </div> */}
+                <div className='w-96 animate-border inline-block rounded-md bg-white bg-gradient-to-r from-red-500 via-green-500 to-yellow-500 bg-[length:400%_400%] p-1' data-aos="flip-left">
+                    <div className=' rounded-md bg-white px-5 py-3 font-bold'>
+                        <h3 className='text-3xl font-bold mb-6 text-center'>Member of</h3>
+                        <div className='flex flex-col justify-center items-center'>
+                            <img src={member1} alt="" />
+                            <div className='w-80'><img src={member2} alt="" /></div>
+                            <img src={member3} alt="" />
+                        </div>
                     </div>
                 </div>
             </div>
