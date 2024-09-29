1d816e0f23ce07bd718011d8c433b9a6d965a790
asib ahmed <ahmedasib1@gmail.com>
2024-09-29 22:42:45 +0600

    fix: scroll to top

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -3,12 +3,14 @@ import { Outlet } from 'react-router-dom';
 import Navbar from '../pages/shared/Navbar/Navbar';
 import Footer from '../pages/shared/Footer/Footer';
 import Logo from '../pages/Home/Logo/Logo';
+import ScrollToTop from '../pages/shared/ScrollToTop';
 
 const Main = () => {
     return (
         <div>
             <Logo></Logo>
             <Navbar></Navbar>
+            <ScrollToTop></ScrollToTop>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
--- src/pages/shared/Footer/Footer.jsx
+++ src/pages/shared/Footer/Footer.jsx
@@ -52,55 +52,6 @@ const Footer = () => {
                 </div>
             </div>
             <div className='bg-[#d3e9d3] py-16 '>
-                {/* <h2 className='text-5xl font-extrabold flex mb-14 items-center justify-center'>Follow Us</h2>
-                <div className='flex justify-between items-center max-w-7xl mx-auto flex-wrap max-[992px]:justify-center max-[992px]:gap-10'>
-                    <div><img src={logo} alt="" /></div>
-                    <div>
-                        <div className='flex justify-around'>
-                            <a>
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="24"
-                                    height="24"
-                                    viewBox="0 0 24 24"
-                                    className="fill-current">
-                                    <path
-                                        d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
-                                </svg>
-                            </a>
-                            <a>
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="24"
-                                    height="24"
-                                    viewBox="0 0 24 24"
-                                    className="fill-current">
-                                    <path
-                                        d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-                                </svg>
-                            </a>
-                            <a>
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="24"
-                                    height="24"
-                                    viewBox="0 0 24 24"
-                                    className="fill-current">
-                                    <path
-                                        d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-                                </svg>
-                            </a>
-                        </div>
-                        <div>
-                            <img className='w-[556px]' src={footImage} alt="" />
-                        </div>
-                    </div>
-                    <div >
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3650.9599409943826!2d90.36527617484678!3d23.784440787469606!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c100504c6233%3A0x507015786bd5545d!2sDCIT%20Ltd!5e0!3m2!1sen!2sbd!4v1725171268137!5m2!1sen!2sbd" width="350" height="225" style={{ border: 0 }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                        <Link to={'https://maps.app.goo.gl/86uan3GWN2QWzwqd7'} className='flex text-lg pt-5'><MapPinIcon className='size-6 p-1 text-[#399918] mr-2' /> Google Map</Link>
-                    </div>
-                </div> */}
-
                 <footer className="footer p-6 max-w-7xl mx-auto text-base">
                     <nav>
                         <h6 className="footer-title text-xl">Head Office</h6>
