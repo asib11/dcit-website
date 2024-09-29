87f6b810151e5bcad622c9058f69fe02c2b75bd5
asib ahmed <ahmedasib1@gmail.com>
2024-09-29 23:35:25 +0600

    fix: navbar responsive

--- src/pages/shared/Navbar/Button.jsx
+++ src/pages/shared/Navbar/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = () => {
   return (
-    <button className="bg-[#399918] text-white  px-6 py-2 rounded-sm">
+    <button className="bg-[#399918] text-white max-lg:px-3 lg:px-6 py-2 rounded-sm">
       Admisssion Now
     </button>
   );
--- src/pages/shared/Navbar/Navbar.jsx
+++ src/pages/shared/Navbar/Navbar.jsx
@@ -9,60 +9,59 @@ const Navbar = () => {
     return (
         <div className="sticky inset-x-0 top-0 z-10 ">
             <nav className="bg-white max-w-7xl mx-auto shadow-xl shadow-gray-700">
-                <div className="flex items-center justify-around">
-                    <div className="z-50 max-lg:py-2 lg:p-5 md:w-auto w-[90%] flex justify-end">
-                        {/* <img src={Logo} alt="logo" className="md:cursor-pointer h-9" /> */}
-                        <div className="text-3xl md:hidden" onClick={() => setOpen(!open)}>
-                            <ion-icon name={`${open ? "close" : "menu"}`}></ion-icon>
+                <div className="lg:flex lg:items-center lg:justify-between max-lg:px-3 lg:px-5">
+                    <div className="flex items-center max-lg:justify-between lg:justify-around">
+                        <div className="z-50 max-lg:py-2 lg:py-5 md:w-auto w-full flex justify-end">
+                            {/* <img src={Logo} alt="logo" className="md:cursor-pointer h-9" /> */}
+                            <div className="text-3xl md:hidden" onClick={() => setOpen(!open)}>
+                                <ion-icon name={`${open ? "close" : "menu"}`}></ion-icon>
+                            </div>
                         </div>
+                        <ul className="md:flex hidden font-semibold items-center lg:gap-4">
+                            <li>
+                                <Link to="/" className="py-7 px-3 inline-block">
+                                    Home
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/about" className="py-2 px-3 inline-block">
+                                    About Us
+                                </Link>
+                            </li>
+                            <NavLinks />
+                            <li>
+                                <Link to="/event" className="py-7 px-3 inline-block">
+                                    Events
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/gallery" className="py-7 px-3 inline-block">
+                                    Gallery
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/rpl" className="py-7 px-3 inline-block">
+                                    RPL
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/news" className="py-7 px-3 inline-block">
+                                    News
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/contact" className="py-7 px-3 inline-block">
+                                    Contact Us
+                                </Link>
+                            </li>
+                        </ul>
                     </div>
-                    <ul className="md:flex hidden font-semibold items-center lg:gap-4">
-                        <li>
-                            <Link to="/" className="py-7 px-3 inline-block">
-                                Home
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/about" className="py-2 px-3 inline-block">
-                                About Us
-                            </Link>
-                        </li>
-                        <NavLinks />
-                        <li>
-                            <Link to="/event" className="py-7 px-3 inline-block">
-                                Events
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/gallery" className="py-7 px-3 inline-block">
-                                Gallery
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/rpl" className="py-7 px-3 inline-block">
-                                RPL
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/news" className="py-7 px-3 inline-block">
-                                News
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/contact" className="py-7 px-3 inline-block">
-                                Contact Us
-                            </Link>
-                        </li>
-                    </ul>
                     <div className="md:block hidden">
                         <Link to={'/contact'}><Button /></Link>
                     </div>
                     {/* Mobile nav */}
                     <ul
-                        className={`
-        md:hidden bg-white fixed w-full top-0 overflow-y-auto bottom-0 py-24 pl-4
-        duration-500 ${open ? "left-0" : "left-[-100%]"}
-        `}
+                        className={`md:hidden bg-white fixed w-full top-0 overflow-y-auto bottom-0 py-24 pl-4 duration-500 ${open ? "left-0" : "left-[-100%]"}`}
                     >
                         <li>
                             <Link to="/" className="py-7 px-3 inline-block">
