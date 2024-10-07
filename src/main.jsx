7182af25deb2f46c983ab07b90753261b78af938
DCIT LTD <dcitltd2022@gmail.com>
2024-10-07 13:50:15 +0600

    test

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import {
 
@@ -10,7 +9,7 @@ import router from './Routes/Routes.jsx';
 
 
 createRoot(document.getElementById('root')).render(
-  <div className=''>
+  <div>
     <StrictMode>
       <RouterProvider router={router} />
     </StrictMode>,
