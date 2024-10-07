c8fb2c52ee62537a163795483049899ea93672ba
asib ahmed <ahmedasib1@gmail.com>
2024-10-07 18:17:59 +0600

    fix: service data upload and component make

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -18,8 +18,8 @@ const Services = () => {
             console.log(data)
         }
     return (
-        <div className='max-w-7xl mx-auto space-y-6 pb-10'>
-            <h1 className='max-lg:text-2xl lg:text-3xl font-bold max-lg:py-8 lg:pt-16'>Get the Best <span className='text-green-600'>{data?.name}</span> in Dhaka Bangladesh </h1>
+        <div className='max-w-7xl mx-auto space-y-6 pb-10 text-justify'>
+            <h1 className='max-lg:text-2xl lg:text-3xl font-bold max-lg:py-8 lg:pt-16'>Best Custom <span className='text-green-600'>{data?.name}</span> </h1>
             <p className='text-xl'>
                 {data?.description}
             </p>
