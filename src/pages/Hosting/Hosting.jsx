14ae3d3758bd69e3dc3532fd3d2ecf90914ca664
asib ahmed <ahmedasib1@gmail.com>
2024-10-09 10:34:07 +0600

    fix: hosting page

--- src/pages/Hosting/Hosting.jsx
+++ src/pages/Hosting/Hosting.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 
 const Hosting = () => {
     const [hosting, setHosting] = useState([]);
+    const {hostingId} = useParams()
 
     // Fetch the MDX content from the JSON file
     useEffect(
@@ -14,13 +16,18 @@ const Hosting = () => {
         }
         , []);
 
+        {
+            console.log(hosting)
+        }
+
+        const dataHosting = hosting.find(item => item.link === hostingId)
 
     return (
         <div className='max-w-7xl mx-auto'>
             <h1>Accounting Software Details</h1>
             <div className='grid max-md:grid-cols-1 justify-items-center gap-6 max-lg:grid max-lg:grid-cols-2 lg:grid lg:grid-cols-4 py-20' >
                 {
-                    hosting.map((host, index) => (
+                    dataHosting?.content?.map((host, index) => (
                         <div key={index} className="card bg-base-100 w-[90%] shadow-xl rounded-md" >
                             <h2 className='font-bold text-2xl text-center bg-red-600 text-white p-10'>{host.name}</h2>
                             <ul className='text-xl p-4 space-y-4'>
