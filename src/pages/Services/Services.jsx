1be0abfa1d89974cf0194989caa676cbda47d990
asib ahmed <ahmedasib1@gmail.com>
2024-10-09 14:12:59 +0600

    fix: hosting page

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -12,11 +12,11 @@ const Services = () => {
                 .catch(error => console.log(error))
         }
         , []);
-        const data = serviceData.find(item => item.link === serviceId)
-        {
-            console.log(serviceId)
-            console.log(data)
-        }
+    const data = serviceData.find(item => item.link === serviceId)
+    {
+        console.log(serviceId)
+        console.log(data)
+    }
     return (
         <div className='max-w-7xl mx-auto space-y-6 pb-10 text-justify'>
             <h1 className='max-lg:text-2xl lg:text-3xl font-bold max-lg:py-8 lg:pt-16'>Best <span className='text-green-600'>{data?.name}</span> </h1>
@@ -33,7 +33,7 @@ const Services = () => {
 
                 {
 
-                    data?.details?.map((item,idx) =>
+                    data?.details?.map((item, idx) =>
                         (<li key={idx}>{item}</li>)
 
                     )
@@ -47,7 +47,7 @@ const Services = () => {
 
                 {
 
-                    data?.details2?.map((item,idx) =>
+                    data?.details2?.map((item, idx) =>
                         (<li key={idx}>{item}</li>)
 
                     )
@@ -61,13 +61,15 @@ const Services = () => {
 
                 {
 
-                    data?.details3?.map((item,idx) =>
+                    data?.details3?.map((item, idx) =>
                         (<li key={idx}>{item}</li>)
 
                     )
                 }
             </ul>
-            <h1 className='max-lg:text-2xl lg:text-3xl text-center font-bold max-lg:py-8 lg:py-16'>For additional features or more details about <span className='text-green-600'>{data?.name}</span>. please call us : +8801710-822207, +8801872-884988 ( 08AM to 10PM )</h1>
+            <h1 className='max-lg:text-2xl lg:text-3xl font-bold max-lg:py-8 lg:py-16 text-gray-500'>
+                For additional features or more details about <span className='text-green-600'>{data?.name}</span>. <br /> please call us : <br /> <span className='text-red-600 italic	'>+8801710-822207, +8801872-884988</span> <br /> ( 08AM to 10PM )
+            </h1>
         </div>
     );
 };
